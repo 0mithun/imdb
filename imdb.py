4e208435db3bf2fe86124b2ac3133d921f6ccecf
Mithun Halder <mithunrptc@gmail.com>
2019-02-06 00:52:19 +0600

    Fixed page calculation logic

--- imdb.py
+++ imdb.py
@@ -8,46 +8,42 @@ from bs4 import BeautifulSoup
 
 class Imdb():
     url = ''
-    total_movie = ''
     per_page = ''
-    start_page = ''
-    end_page = ''
+    start = ''
+    end = ''
     total_page = 0
     movie_links = []
-    def __init__(self, url, total_movie, per_page=50, start_page=1,end_page=''):
+    def __init__(self, url, per_page=50, start=0,end=0):#1005, 1007
             self.url=url
-            self.total_movie = total_movie
             self.per_page = per_page
-            self.start_page = start_page
-            self.end_page = end_page
-
-    def calculate_total_page(self):
-        if((self.total_movie % self.per_page) == 0 ):
-            self.total_page = self.total_movie / self.per_page
-        else:
-            self.total_page = (self.total_movie // self.per_page)+1
+            self.start = start
+            self.end = end
 
     def browse_all_page(self):
-        self.calculate_total_page()
-        for i in range(self.start_page, self.total_page+1):
+        for i in range(self.start, self.end, self.per_page):
             url = self.url + "&start={}".format(i)
             html = requests.get(url).content 
             soup = BeautifulSoup(html,'lxml')
+            print(url)
             self.extract_movies_url(soup)
-            
+         
     def extract_movies_url(self, data):
         movies = data.find_all('div', class_="lister-item")
-        total_movies = len(movies)
-        print(total_movies)
+        
         for movie in movies:
+            serial = movie.find('span', class_="lister-item-index unbold text-primary").text
+            serial = int(serial.replace('.','').replace(',',''))
+            if(serial >self.end):
+                break
             link = "https://www.imdb.com"+ movie.find('div', class_="lister-item-image").a['href']
+            self.movie_links.append(link)
             print(link)
 
 
 
-        
-imdb = Imdb(url='https://www.imdb.com/search/title?user_rating=8.0', per_page=50, total_movie=45)
 
+        
+imdb = Imdb(url='https://www.imdb.com/search/title?user_rating=8.0', per_page=50, start=1005,  end=1018)
 
 imdb.browse_all_page()
 
