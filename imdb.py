f8316c0e4235e654b77af0a440cffa81cc6a1fc9
Mithun Halder <mithunrptc@gmail.com>
2019-02-08 02:16:33 +0600

    Complete Imdb Project

--- imdb.py
+++ imdb.py
@@ -1,75 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+
+total_page = 0
+movie_links = []
+movie_details = []
 
 
-class Imdb():
-    url = ''
-    per_page = ''
-    start = ''
-    end = ''
-    total_page = 0
-    movie_links = []
-    def __init__(self, url, per_page=50, start=1,end=0):#1005, 1007
-            self.url=url
-            self.per_page = per_page
-            self.start = start
-            self.end = end
-
-    def browse_all_page(self):
-        for i in range(self.start, self.end, self.per_page):
-            url = self.url + "&start={}".format(i)
-            html = requests.get(url).content 
-            soup = BeautifulSoup(html,'lxml')
-            print(url)
-            self.extract_movies_url(soup)
-         
-    def extract_movies_url(self, data):
-        movies = data.find_all('div', class_="lister-item")
+def browse_all_page(url, start=1, end=0, per_page=50):
+    for i in range(start, end, per_page):
+        url = url + "&start={}".format(i)
+        html = requests.get(url).content 
+        soup = BeautifulSoup(html,'lxml')
+        extract_movies_url(soup, end)
+    
         
-        for movie in movies:
-            serial = movie.find('span', class_="lister-item-index unbold text-primary").text
-            serial = int(serial.replace('.','').replace(',',''))
-            if(serial >self.end):
-                break
-            link = "https://www.imdb.com"+ movie.find('div', class_="lister-item-image").a['href']
-            self.movie_links.append(link)
+def extract_movies_url(data, end):
+    movies = data.find_all('div', class_="lister-item")    
+    for movie in movies:
+        serial = movie.find('span', class_="lister-item-index unbold text-primary").text
+        serial = int(serial.replace('.','').replace(',',''))
+        if(serial > end):
+            break
+        link = "https://www.imdb.com"+ movie.find('div', class_="lister-item-image").a['href']
+        movie_links.append(link)
 
-    def get_movie_info(self):
-        for movie in self.movie_links:
+def write_csv(data, filename="file.csv"):
+    with open(filename, 'w', newline='') as csvfile:
+        f = csv.writer(csvfile)
+        for line in data:
+            f.writerow([line['title'],line['year'],line['director'],line['rating'], line['metascore'], line['budget']])
+
+def get_movie_details(movie_links):
+        for movie in movie_links:
             html = requests.get(movie).content
             soup = BeautifulSoup(html,'lxml')
-
             title = soup.find('div', class_="title_wrapper").h1.text
-            
             year = title[-6:-2]
             title = title[:-7].strip()
             rating = soup.find('span', attrs={'itemprop':'ratingValue'}).text
             director = soup.find('div',class_="credit_summary_item").a.text.strip()
-            metascore = soup.find('span', class_="metascore")
-            
+            metascore = soup.find('div', class_="metacriticScore")
             if(metascore == None):
-                metascore = "No Metascore Found"
-            print(director)
-            print(year)
-            print(title)
-            print(rating)
-            print('-'*10)
+                metascore = "No Metascore"
+            else:
+                metascore = soup.find('div', class_="metacriticScore").text.strip()              
+            box = soup.find('h4', text="Budget:")            
+            if(box == None):
+                budget = "No Budget Found"
+            else:
+                budget = box.parent.text.strip().split('\n')[0]
+                try:
+                    budget = budget.split('$')[1]
+                except IndexError:
+                    budget = budget.split('\xa0')[1]
+            
+            data = {'title': title,'year':year,'director':director,'rating':rating,'metascore':metascore,'budget':budget}
+            movie_details.append(data)
 
 
 
+browse_all_page(url="https://www.imdb.com/search/title?title_type=feature&user_rating=8.0,", start=1, end=100)
 
-        
-imdb = Imdb(url='https://www.imdb.com/search/title?user_rating=8.0', per_page=50, end=5)
+get_movie_details(movie_links)
 
-imdb.browse_all_page()
-
-imdb.get_movie_info()
-
-
-
-
-
-        
-
-        
-
+write_csv(data=movie_details, filename='movies.csv')
